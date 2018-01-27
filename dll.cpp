cb3c631501409dede2b92b1cc4bb4c1f96d10156
paularnav <31165622+paularnav@users.noreply.github.com>
2018-01-27 10:42:13 +0530

    Update dll.cpp

--- dll.cpp
+++ dll.cpp
@@ -1,4 +1,5 @@
 # lab-3-sem-2-
+
 #include<iostream>
 using namespace std;
 
@@ -6,6 +7,7 @@ class Node {//element of the linked list
 	public:
 		int data;
 		Node* next;
+		Node* prev;
 
 		Node() {//constuctor initializes an empty node
 			data = 0;
@@ -32,6 +34,7 @@ class LinkedList {
 		void deleteAt(int i);
 		void display();
 		int countElements();
+		void reversedisplay();
 };
 
 void LinkedList::insert(int x) {
@@ -43,10 +46,10 @@ void LinkedList::insert(int x) {
             //Checks if the list is empty or not and changes head or attaches the new node to the list accordingly
 	if (head == NULL) {
 		head = temp;
-	} else {//attaches the new last element
+		temp->prev=NULL;
+	} else {//attaches the new node after the last element through double link
 		tail->next = temp;
-		//attaches last element back to the head making it a circular linked list
-		temp->next=head;
+		temp->prev=tail;
 	}
 	//changes the tail and increments the number of elements by one
 		tail = temp;
@@ -61,14 +64,15 @@ if (i > n)
 	//adds the element to the start,assigns the next of temp to the first element and changes the head to point at the new start
 else if (i == 0) 
   {Node* temp = new Node;
-   //adds new head
 		temp->data = x;
+   //adds the element to the start of the list and makes the previous null, changes the value of head
 		temp->next = head;
+		temp->prev=NULL;
 		head = temp;
 		++n;
 	} 
 else if (i == n) 
-  {//adds the node at the end of the list, uses the insert function
+  {//adds the node at the end of the list, identical to insert function
   	
   	insert(x);
 	 
@@ -80,30 +84,28 @@ else if (i == n)
 
 		Node* locate = head;
                         //pointer variable locate transverses through the list to find position i-1
-		for (int j=0; j<i-1; ++j) {
+		for (int j=0; j<i-2; ++j) {
 			locate = locate->next;
 		}
                         //the new node points to the node which the (i-1)th node was pointing to, which in turn points to the new node 
-		//attaches temp to the next node in the list
-	        temp->next = locate->next;
-	  //attaches the last second element to the new node
+	//temp is inserteed after locate with double link	
+	  temp->next = locate->next;
+		locate->next->prev=temp;
 		locate->next = temp;
+		temp->prev=locate;
 		//increases the elements by one, as the node is successfully added
 		++n;
 	}
 }
 
 void LinkedList::deletelast()
-{                       Node* locate = head;
-		//locates the last second element
-		for (int j=0; j<n-2; ++j) {
-			locate = locate->next;
-		}
-		//tail points to the new end, and the previous one is removed from the list
-		tail = locate;
- //the circular link is re-established
-		locate->next = head;
-		--n;
+{      Node* temp;
+       temp=tail;
+ //temp replaces tail and is attached back to the head
+       tail=temp->prev;
+       tail->next=NULL;
+       delete temp;
+       n--;
 }
 
 void LinkedList::deleteAt(int i) {
@@ -111,18 +113,31 @@ void LinkedList::deleteAt(int i) {
 		cout << "Linked List does not have that many elements \n";
 	} else if (i == 0) {
 		//changes the head so that the first element is no longer in the list
+		Node*temp;
+		temp=head;
+		//head shifts to next
 		head = head->next;
+		//the previous is null-ed
+		head->prev=NULL;
+		//deletes the temp
+		delete temp;
 		--n;
 	} else if (i == n-1) {
 		//the tail shifts to the element before the last and the last element is detached from the list
 		deletelast();
 	} else {    //locate pointer locates the (i-1)th element and makes it point to the (i+1)th element hence detaching the ith element from the list
 		Node* locate = head;
-		for (int j=0; j<i-1; ++j) {
+		Node* temp;
+		//locate transverses the list to find the element before the one to be deleted
+		for (int j=0; j<i-2; ++j) {
 			locate = locate->next;
 		}
-                //locate is made to point to the second element to locate in the list
+        temp=locate->next;
+		//new node points to the second element after it
 		locate->next = (locate->next)->next;
+		//it points back to the element before it removing the last one 
+		locate->next->prev=locate;
+		delete temp;
 		--n;
 	}
 }
@@ -134,35 +149,46 @@ void LinkedList::display() {
 		Node* temp = new Node;
 		temp = head;
 		//temp pointer transverses through the list printing all elements
-		while (temp != tail) {
-			cout << temp->data <<" -> ";
+		while (temp != NULL) {//prints all data elements
+			cout << temp->data<<" -> ";
 			temp = temp->next;
-		}//prints the tail
-		cout<<tail->data;
+		}cout<<endl;
+		//delets temp pointer
+		delete temp;
 	}
+}
+
+void LinkedList::reversedisplay() {
+	Node* temp = new Node;
+		temp = tail;
+		//temp pointer transverses through the list printing all elements from tail to head in reverse
+		while (temp != NULL) {
+			cout << temp->data <<" -> ";
+			temp = temp->prev;}
+			cout<<endl;
+			delete temp;
 }
 
 int LinkedList::countElements()
 {int j;
 j=n;
- //puts the value of n in the variable j and returns it's value
 	return j;}
 
 
 int main() {
 	LinkedList list1;
 	int a=0;
-	cout<<"Enter the number associated to the operation you want to do \n1.addnode\n2.add node at a certain position\n3.delete node\n4.Delete a particular element\n5.Display elements of the list\n6.Count elements in the list\n7.Exit\n";
-	while(a!=7)
+	cout<<"Enter the number associated to the operation you want to do \n1.addnode\n2.add node at a certain position\n3.delete node\n4.Delete a particular element\n5.Display elements of the list\n6.Count elements in the list\n7.Displays the elements in reverse\n8.Exit\n";
+	while(a!=8)
 	{cin>>a;
 	if(a==1)
-	{cout<<"Enter the element to be inserted\n";
+	{cout<<"Enter the element to be inserted"<<endl;
 	int x;
 	cin>>x;
 	list1.insert(x);
 	}
 	else if(a==2)
-	{cout<<"Enter the element to be inserted and it's position\n";
+	{cout<<"Enter the element to be inserted and it's position\n"<<endl;
 	int x;
 	int i;
 	cin>>x>>i;
@@ -182,10 +208,13 @@ int main() {
 	else if(a==6)
 	{int n;
 	n=list1.countElements();
-	cout<<"The number of elements are "<<n;
+	cout<<"The number of elements are "<<n<<endl;
 	}
+	else if (a==7)
+	{list1.reversedisplay();}
 		
 }
+
 	
 	return 0;
 }
